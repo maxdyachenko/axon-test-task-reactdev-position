3f00b6ff687419320265d889a6fb69c711763b7e
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-07-23 12:48:38 +0300

    add stateless app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Table from './containers/Table/TableContainer'
 import Form from './containers/Form/FormContainer'
 import Summary from './containers/Summary/SummaryContainer'
 
-class App extends Component {
-    state = {};
-
-    render() {
-        return (
-            <div>
-                <Table />
-                <Summary />
-                <Form />
-            </div>
-        );
-    }
-}
+const App = () => (
+    <div>
+        <Table />
+        <Summary />
+        <Form />
+    </div>
+);
 
 export default App;
